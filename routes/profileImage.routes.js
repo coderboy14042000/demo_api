10647ce279a08ce47bfc83067806a886ebd21b3f
Mohit Panchal <mohit.panchal@zealousys.local>
2022-07-05 16:34:37 +0530

    add comment in profileImage.routes.js file

--- routes/profileImage.routes.js
+++ routes/profileImage.routes.js
@@ -5,6 +5,6 @@ const imageUploader=require('../helper/imageUpload.helper');
 let Authentication=require("../helper/auth.helper");
 
 /* GET uploadProfileImage router listing. */
+//imageUploader.upload.single('profile_image') is use to upload single file.
 router.post('/uploadProfileImage/:id',Authentication.ensure,imageUploader.upload.single('profile_image'),profileImageController.uploadProfileImage);
-
 module.exports = router;
